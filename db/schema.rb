a05170977d5a3c37fb267bf3e3f01249b374dbff
Sergian <serpriest@yahoo.com>
2016-05-02 00:26:54 +0000

    saving the price column I hope

--- db/schema.rb
+++ db/schema.rb
@@ -20,7 +20,7 @@ ActiveRecord::Schema.define(version: 20160428094453) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.string   "colour"
-    t.integer "price"
+    t.string   "price"
   end
 
 end
