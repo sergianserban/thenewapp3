7702c2436be2f209614a2abe4a0b67fa10dd6895
Sergian <serpriest@yahoo.com>
2016-05-02 00:05:31 +0000

    recreating again the price column

--- db/migrate/20160417140616_create_products.rb
+++ db/migrate/20160417140616_create_products.rb
@@ -4,6 +4,7 @@ class CreateProducts < ActiveRecord::Migration
       t.string :name
       t.text :description
       t.string :image_url
+      t.integer :price
       t.timestamps null: false
     end
   end
